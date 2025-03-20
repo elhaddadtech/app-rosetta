bdbfe8f8ef5bade7f716ebbbedaed31a1b83428e
Elhaddad Abdelmounaim <a.elhaddad@uca.ac.ma>
2025-03-20 15:11:09 +0000

    add Authentication to all routs

--- app/Http/Controllers/api/RegisterController.php
+++ app/Http/Controllers/api/RegisterController.php
@@ -1,75 +1,70 @@
 <?php
 
 namespace App\Http\Controllers\api;
-use App\Http\Controllers\API\BaseController as BaseController;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Validator;
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Http\JsonResponse;
-use App\Http\Controllers\Controller;
 
-class RegisterController extends BaseController
-{
-    /**
-     * Register api
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function register(Request $request): JsonResponse
-    {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required',
-            'email' => 'required|email',
-            'password' => 'required',
-            'c_password' => 'required|same:password',
-        ]);
-   
-        if($validator->fails()){
-            return $this->sendError('Validation Error.', $validator->errors());       
+class RegisterController extends Controller {
+  /**
+   * Register api
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function register(Request $request): JsonResponse {
+    $validator = Validator::make($request->all(), [
+      'name'       => 'required',
+      'email'      => 'required|email',
+      'password'   => 'required',
+      'c_password' => 'required|same:password',
+    ]);
+
+    if ($validator->fails()) {
+      return response()->json(['error' => $validator->errors()], 401);
+    }
+
+    $input             = $request->all();
+    $input['password'] = bcrypt($input['password']);
+    $user              = User::create($input);
+    $success['token']  = $user->createToken('App_Rosetta')->plainTextToken;
+    $success['name']   = $user->name;
+
+    return response()->json($success);
+  }
+
+  /**
+   * Login api
+   *
+   * @return \Illuminate\Http\Response
+   */
+
+  public function login(Request $request): JsonResponse {
+    $user = User::where('email', $request->email)->first();
+
+    if ($user) {
+      if ($request->filled('password')) {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+          // Authentication successful
+          $user = Auth::user();
+        } else {
+          return response()->json(['error' => 'Unauthorized'], 401);
         }
-   
-        $input = $request->all();
-        $input['password'] = bcrypt($input['password']);
-        $user = User::create($input);
-        $success['token'] =  $user->createToken('App_Rosetta')->plainTextToken;
-        $success['name'] =  $user->name;
-   
-        return $this->sendResponse($success, 'User register successfully.');
-    }
-   
-    /**
-     * Login api
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function login(Request $request): JsonResponse
-    {
-        // if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
-            // $user = Auth::user(); 
+      } else {
+        // Authenticate with email only
+        Auth::login($user);
+      }
 
-        $user = User::where('email', $request->email)->first();
-        if($user ){ 
-            if ($request->filled('password')) {
-                if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
-                    // Authentification réussie avec mot de passe
-                    $user = Auth::user();
-                } else {
-                    // Si le mot de passe est incorrect
-                    return $this->sendError('Unauthorised.', ['error' => 'Invalid password.']);
-                }
-            } else {
-                // Authentification avec email uniquement (sans mot de passe)
-                Auth::login($user); // Authentification manuelle
-            }
-            $success['token'] =  $user->createToken('App_Rosetta')->plainTextToken; 
-            $success['firstname'] =  $user->firstname;
-            $success['lastname'] =  $user->lastname;
-   
-            return $this->sendResponse($success, 'User login successfully.');
-        } 
-        else{ 
-            return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
-        } 
+      // Create a token
+      $token = $user->createToken('API Token')->plainTextToken;
+
+      return response()->json(['token' => $token, 'firstname' => $user->first_name, 'lastname' => $user->last_name]);
+    } else {
+      return response()->json(['error' => 'Unauthorized'], 401);
     }
+  }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -24,52 +24,54 @@ Route::controller(RegisterController::class)->group(function () {
   Route::post('login', 'login');
 });
 
-Route::apiResource('roles', RoleController::class);
-Route::middleware('auth:sanctum')->group(function () {});
-Route::apiResource('users', UserController::class);
-Route::get('appusers', [UserController::class, 'appUsers']);
-Route::apiResource('groups', GroupController::class);
-Route::apiResource('languages', LanguageController::class);
-Route::apiResource('teachers', TeacherController::class);
-Route::apiResource('chiefs', ChiefController::class);
-Route::apiResource('courses', CoursController::class);
-Route::apiResource('range-cefefrs', RangeCefefrController::class);
-Route::apiResource('cefr-mappings', CefrMappingController::class);
-Route::get('results', [ResultController::class, 'index']); // Get all results
-Route::post('results', [ResultController::class, 'show']); // Accepts only alphabetic strings (no numbers or special characters); // Get result by ID
-Route::post('results/institution', [ResultController::class, 'searchByInstitution']); // Accepts only alphabetic strings (no numbers or special characters); // Get result by ID
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+  return $request->user();
+});
+
+// Group all protected routes under auth:sanctum middleware
+Route::middleware('auth:sanctum')->group(function () {
+  Route::apiResource('roles', RoleController::class);
+  Route::apiResource('users', UserController::class);
+  Route::get('appusers', [UserController::class, 'appUsers']);
+  Route::apiResource('groups', GroupController::class);
+  Route::apiResource('languages', LanguageController::class);
+  Route::apiResource('teachers', TeacherController::class);
+  Route::apiResource('chiefs', ChiefController::class);
+  Route::apiResource('courses', CoursController::class);
+  Route::apiResource('range-cefefrs', RangeCefefrController::class);
+  Route::apiResource('cefr-mappings', CefrMappingController::class);
+  Route::get('results', [ResultController::class, 'index']); // Get all results
+  Route::post('results', [ResultController::class, 'show']); // Accepts only alphabetic strings (no numbers or special characters); // Get result by ID
+  Route::post('results/institution', [ResultController::class, 'searchByInstitution']); // Accepts only alphabetic strings (no numbers or special characters); // Get result by ID
 //----------------------Search Api ----------------------------
-Route::post('search/user', [SearchController::class, 'searchUsers']);
-Route::get('students/stats', [SearchController::class, 'StatStudents']);
-Route::post('students/institution', [SearchController::class, 'searchByInstitution']);
-Route::post('students/search', [SearchController::class, 'searchByParams']);
+  Route::post('search/user', [SearchController::class, 'searchUsers']);
+  Route::get('students/stats', [SearchController::class, 'StatStudents']);
+  Route::post('students/institution', [SearchController::class, 'searchByInstitution']);
+  Route::post('students/search', [SearchController::class, 'searchByParams']);
 //-------------Import CSV Files ------------------------
-Route::post('/students/import', [ImportController::class, 'importStudents']);
-Route::post('/teachers/import', [TeacherController::class, 'importTeachers']);
-Route::post('/Groups/import', [GroupController::class, 'importGroups']);
+  Route::post('/students/import', [ImportController::class, 'importStudents']);
+  Route::post('/teachers/import', [TeacherController::class, 'importTeachers']);
+  Route::post('/Groups/import', [GroupController::class, 'importGroups']);
 //import csv file learnerGrowth
-Route::post('import/LearnerGrowthReport', [LeanerGrowthReportController::class, 'importGrowthCSV']);
-Route::post('import/LearnerGrowthReport/handle', [LeanerGrowthReportController::class, 'handle']);
+  Route::post('import/LearnerGrowthReport', [LeanerGrowthReportController::class, 'importGrowthCSV']);
+  Route::post('import/LearnerGrowthReport/handle', [LeanerGrowthReportController::class, 'handle']);
 // import file FlencyBuilder
-Route::post('import/builderReport', [BuilderController::class, 'importBuilderCSV']);
-Route::post('import/builderReport/handle', [BuilderController::class, 'handle']);
+  Route::post('import/builderReport', [BuilderController::class, 'importBuilderCSV']);
+  Route::post('import/builderReport/handle', [BuilderController::class, 'handle']);
 //import Fondation File
-Route::post('import/FondationReport', [FondationReportController::class, 'importFondationCSV']);
-Route::post('import/FondationReport/handle', [FondationReportController::class, 'handle']);
+  Route::post('import/FondationReport', [FondationReportController::class, 'importFondationCSV']);
+  Route::post('import/FondationReport/handle', [FondationReportController::class, 'handle']);
 
 //---------------Export CSV Files ------------------------
 
-Route::get('builderReport/export', [BuilderController::class, 'exportToExcel']);
-Route::post('students/export', [UserController::class, 'exportStudents']);
+  Route::get('builderReport/export', [BuilderController::class, 'exportToExcel']);
+  Route::post('students/export', [UserController::class, 'exportStudents']);
 
-Route::get('learnerGrowth/export', [LeanerGrowthReportController::class, 'ExportDataLearnerGrowth']);
-Route::get('learnerGrowth/results/export', [LeanerGrowthReportController::class, 'exportResultsToCsv']);
-Route::get('courses/export/{institution}/{branch?}', [BuilderController::class, 'exportCourseToCsv']);
+  Route::get('learnerGrowth/export', [LeanerGrowthReportController::class, 'ExportDataLearnerGrowth']);
+  Route::get('learnerGrowth/results/export', [LeanerGrowthReportController::class, 'exportResultsToCsv']);
+  Route::get('courses/export/{institution}/{branch?}', [BuilderController::class, 'exportCourseToCsv']);
 //Coures controller
 // --------------Results_Stats --------------------------------
-Route::get('learnerGrowth/stats', [ResultsStatsControler::class, 'exportLearnerGrowthToCsv']);
-Route::get('coures/notes/{institution}/{file}', [CoursController::class, 'calculateNotes']);
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+  Route::get('learnerGrowth/stats', [ResultsStatsControler::class, 'exportLearnerGrowthToCsv']);
+  Route::get('coures/notes/{institution}/{file}', [CoursController::class, 'calculateNotes']);
+});
